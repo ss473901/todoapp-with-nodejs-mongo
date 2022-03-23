1f50cb91d6246b8711b3981c5a9e913667b464c0
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-03-23 18:45:27 +0900

    complete  featers/axios

--- public/script.js
+++ public/script.js
@@ -1,12 +1,19 @@
 const tasksDOM = document.querySelector(".tasks");
 const formDOM = document.querySelector(".task-form");
 const taskInputDOM = document.querySelector(".task-input");
+const formAlertDOM = document.querySelector(".form-alert");
 
 //タスクを全て表示する
 // /api/v1/tasksからタスクを読み込む
 const showTasks = async () => {
   try {
     const { data: tasks } = await axios.get("/api/v1/tasks");
+    //タスクが1つもない時
+    if (tasks.length < 1) {
+      tasksDOM.innerHTML = `<h5 class="empty-list">タスクがありません</h5>`;
+      return;
+    }
+
     //タスクを出力
     const allTasks = tasks
       .map((task) => {
@@ -48,9 +55,20 @@ formDOM.addEventListener("submit", async (e) => {
     showTasks();
     //フォームの中身を空にする
     taskInputDOM.value = "";
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = "タスクを追加しました";
+    formAlertDOM.classList.add("text-success");
   } catch (err) {
     console.log(err);
+    formAlertDOM.style.display = "block";
+    //20文字以上打ち込んだ場合のエラー
+    formAlertDOM.innerHTML = "無効です。もう一度やり直してください。";
   }
+  //3秒後にエラーメッセージを消す
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+    formAlertDOM.classList.remove("text-success");
+  }, 3000);
 });
 
 //タスクを削除する
